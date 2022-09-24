064e71376c3f51e2ef9755ea4397822156e44d9d
Kristen Smith <kristennlynne@gmail.com>
2022-09-24 13:11:15 -0700

    added tailwind styling to partial/header and hero section in index.ejs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@
 module.exports = {
   content: ["./views/**/*.ejs"],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [],
+    themes: ["garden"],
   },
 }
