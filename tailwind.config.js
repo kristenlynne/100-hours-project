4e7ed83d26ab90abfb3780d41772294a7488e254
Kristen Smith <kristennlynne@gmail.com>
2023-01-05 03:02:02 -0700

    added daisyui back as that didn't solve deployment issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
     },
     extend: {},
   },
-  // plugins: [require("daisyui")],
-  // daisyui: {
-  //   themes: ["garden"],
-  // },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["garden"],
+  },
 }
